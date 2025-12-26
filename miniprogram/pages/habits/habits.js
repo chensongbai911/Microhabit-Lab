8b4633158f3a7df377e1591d7659b1cadc289115
Developer <dev@example.com>
2025-12-26 16:19:08 +0800

    fix: 统一按钮样式和优雅空数据提示

--- miniprogram/pages/habits/habits.js
+++ miniprogram/pages/habits/habits.js
@@ -44,7 +44,7 @@ Page({
    * 切换Tab
    */
   switchTab (e) {
-    const index = typeof e === 'number' ? e : e.currentTarget.dataset.index;
+    const index = typeof e === 'number' ? e : parseInt(e.currentTarget.dataset.index, 10);
     this.setData({ currentTab: index });
 
     if (index === 1) {
